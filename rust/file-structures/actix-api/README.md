f9406a8da6e3f2e950fc05f4f6660471918c93b3
Joao Wiederkehr <jgpwieder@gmail.com>
2023-11-09 05:38:11 -0300

    Add docker with Postgres integration

--- rust/file-structures/actix-api/README.md
+++ rust/file-structures/actix-api/README.md
@@ -21,3 +21,28 @@ cd to the project's directoty and run:
 ```sh
 ./target/debug/actix-api
 ```
+
+## Run docker:
+
+```sh
+ docker-compose up --build
+```
+
+
+## Run postegris:
+```sh
+docker run -p 5432:5432 -d \
+    -e POSTGRES_PASSWORD=postgres \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_DB=stripe-example \
+    -v pgdata:/var/lib/postgresql/data \
+    postgres
+```
+
+```sh
+# connect
+psql stripe-example -h localhost -U postgres
+
+docker exec -it bdca2b8c09b7 psql -U postgres stripe-example
+```
+source: https://www.youtube.com/watch?v=G3gnMSyX-XM
--- rust/file-structures/actix-api/src/main.rs
+++ rust/file-structures/actix-api/src/main.rs
@@ -1,4 +1,7 @@
 mod api;  // The compiler will look for the mod.rs file inside the api folder
+use postgres::{Client, NoTls};
+use postgres::Error as PostgresError;
+
 
 use api::task::{
     get_task
@@ -12,6 +15,11 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    if let Err(e) = set_database() {
+        println!("Error: {}", e);
+        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Database error"))
+    }
+
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
@@ -21,4 +29,19 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 80))?
     .run()
     .await
+}
+
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect("postgresql://localhost:5432", NoTls)?;
+
+    client.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )",
+        &[]
+    )?;
+
+    Ok(())
 }
